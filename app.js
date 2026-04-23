4e86703d4d3f274a6187e6bb351659f0a1e22651
Davit Hakobyan <davithkb@icloud.com>
2026-04-23 14:31:56 +0400

    Add catchup endpoint for missed delta's and add cronjob to run automatically

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 import { app, errorHandler } from 'mu';
 import bodyParser from 'body-parser';
+import cron from 'node-cron';
 
 import * as deltaUtil from './lib/delta-util';
 import { subjectIsTypeInGraph } from './util-queries';
-import { syncDocsForSubjectInGraph } from './case-doc-queries';
+import { syncDocsForSubjectInGraph, syncAllCaseDocs } from './case-doc-queries';
 import { ALLOWED_DELTA_SIZE, GRAPH, UPDATEABLE_PREDICATES, WATCH_TYPES } from './config';
+
+const CRON_PATTERN = process.env.CRON_PATTERN || '0 0 * * *';
+
+cron.schedule(CRON_PATTERN, async () => {
+  try {
+    console.log(`[cron] Running case-documents sync (pattern: ${CRON_PATTERN})...`);
+    await syncAllCaseDocs();
+    console.log('[cron] Case-documents sync completed.');
+  } catch (err) {
+    console.trace(err);
+  }
+});
+
+app.post('/run', async (req, res, next) => {
+  try {
+    console.log('Running case-documents sync...');
+    await syncAllCaseDocs();
+    console.log('Case-documents sync completed.');
+    return res.status(200).send({ message: 'Case-documents sync completed.' });
+  } catch (err) {
+    console.trace(err);
+    const error = new Error(err.message || 'Something went wrong while running case-documents sync.');
+    error.status = 500;
+    return next(error);
+  }
+});
 
 app.post('/delta', bodyParser.json({ limit: ALLOWED_DELTA_SIZE }), async (req, res) => {
   res.status(202).end();
